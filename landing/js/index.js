02b35b6cab4e1446ba3fcdda348e3f88cb535b6c
Dimitri <Dimitri.vildina@gmail.com>
2019-07-18 13:08:48 +0200

    first commit anim

---
+++ landing/js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(window).scroll(function(){
+        if($(window).scrollTop()>300){
+            $(".listSocial").fadeOut();
+            $(".socialBar").addClass("fixed");
+        } else{
+            $(".listSocial").fadeIn();
+            $(".socialBar").removeClass("fixed");
+        }
+    });
+});
