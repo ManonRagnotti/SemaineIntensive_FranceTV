70d770fc8f139819667aa2e6fc28e89416a5c957
Morgan Vavasseur <morgan.vavasseur@gmail.com>
2019-07-19 13:53:54 +0200

    add slider

--- landing/js/index.js
+++ landing/js/index.js
@@ -9,3 +9,22 @@ $(document).ready(function() {
         }
     });
 });
+
+var mySwiper = new Swiper ('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    renderBullet: function (index, className) {
+          return '<span class="numero">' + (index + 1) + '</span>';
+        },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
